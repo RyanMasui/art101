187e5c294d650860c47b3d04b969a1c79aeb338e
RyanMasui <50430681+RyanMasui@users.noreply.github.com>
2021-01-27 14:50:03 -0800

    Finished Lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+ * Author: Johnny Luo and Ryan Masui
+ * Created: January 21 2021
+ * Lab 5
+ * License: Public Domain
+*/
+
+//Define Variables for Our Car
+make = "Audi";
+model = "Rs7";
+color = "Sonic Gray";
+year = "2017";
+
+//calcuate age of the car
+age = 2021 - year;
+
+// output of the variables that were assigned.
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
